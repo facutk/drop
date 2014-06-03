308372b0ab3660d4d2ef5918d7a73f11a4c782ea
unknown <facundo.tkaczyszyn@ARBUEWRK039.politec.com.ar>
2014-06-03 13:06:01 -0300

    Change in CORS

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 import os
-from flask import Flask, jsonify
-from flask.ext.cors import *
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -19,8 +18,20 @@ tasks = [
     }
 ]
 
+@app.after_request
+def add_cors(resp):
+    """ Ensure all responses have the CORS headers. This ensures any failures are also accessible
+        by the client. """
+    resp.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin','*')
+    resp.headers['Access-Control-Allow-Credentials'] = 'true'
+    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
+    resp.headers['Access-Control-Allow-Headers'] = request.headers.get( 'Access-Control-Request-Headers', 'Authorization' )
+    # set low for debugging
+    if app.debug:
+        resp.headers['Access-Control-Max-Age'] = '1'
+    return resp
+
 @app.route('/todo/api/v1.0/tasks', methods = ['GET'])
-@cross_origin()
 def get_tasks():
     return jsonify( { 'tasks': tasks } )
 
