4c1eab487bd89a972cf7f6ab993cccb0e3980712
unknown <facundo.tkaczyszyn@ARBUEWRK039.politec.com.ar>
2014-06-25 11:27:27 -0300

    added Timestamp

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-import os
+import os, datetime, calendar
 from flask import Flask, jsonify, request, abort, make_response, url_for
 from werkzeug.contrib.cache import SimpleCache
 
@@ -11,22 +11,24 @@ tasks = [
         'id': 1,
         'title': u'Buy groceries',
         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
+        'done': False,
+        'timestamp': datetime.datetime.utcnow()
     },
     {
         'id': 2,
         'title': u'Learn Python',
         'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
+        'done': False,
+        'timestamp': datetime.datetime.utcnow()
     }
 ]
-
-greetings = 45
 
 def make_public_task(task):
     new_task = {}
     for field in task:
         new_task[field] = task[field]
+        if field == 'timestamp':
+            new_task['timestamp'] = calendar.timegm( task['timestamp'].utctimetuple() )
         if field == 'id':
             new_task['uri'] = url_for('get_task', task_id = task['id'], _external = True)
     return new_task
@@ -67,7 +69,8 @@ def create_task():
         'id': tasks[-1]['id'] + 1,
         'title': request.json['title'],
         'description': request.json.get('description', ""),
-        'done': False
+        'done': False,
+        'timestamp': datetime.datetime.utcnow()
     }
     tasks.append(task)
     return jsonify( { 'task': make_public_task(task) } ), 201
@@ -88,6 +91,7 @@ def update_task(task_id):
     task[0]['title'] = request.json.get('title', task[0]['title'])
     task[0]['description'] = request.json.get('description', task[0]['description'])
     task[0]['done'] = request.json.get('done', task[0]['done'])
+    task[0]['timestamp'] = datetime.datetime.utcnow()
     return jsonify( { 'task': make_public_task(task[0]) } )
     
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['DELETE'])
